f7a771bf7a2a094c37f93c9c2547d929a5f5c974
AutumnTiger2878 <franmora2783@gmail.com>
2024-03-21 11:05:12 -0600

    Hoa

--- src/Tarea_switch_semana_dia.js
+++ src/Tarea_switch_semana_dia.js
@@ -48,6 +48,7 @@ function dia_lunes() {
     document.querySelector('#header').innerHTML = mensaje;
 
   }
+  
 
 
 
---
+++ src/while.js
@@ -0,0 +1,25 @@
+document.querySelector('#header').innerHTML = "Estas en el documento While.js"
+frutas = ["Fresas", "Manzanas", "Mangos", "Peras", "Cerezas", "Mandarinas"]
+
+
+var i = 5
+
+while(i >= 0){
+
+    console.log(i +". " +frutas[i])
+    i = i - 1
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
