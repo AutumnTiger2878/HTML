88432fbe129769f03f71638fc6b7750a8219a85c
AutumnTiger2878 <franmora2783@gmail.com>
2024-04-05 11:10:27 -0600

    Cosas

--- src/cesta_app.js
+++ src/cesta_app.js
@@ -2,10 +2,47 @@ document.querySelector('#header').innerHTML = "Estoy en el documento array"
 
 
 
+
+
+function VerCesta(){
+    prompt(cesta)
+
+}
+function Agregar(){
+
+
+
+
+}
+function Eliminar(){
+
+
+
+
+}
+
+
+
+
+
+
+
+i = 0
+
+var cesta = []
+
+
+
+
+
+do{
 var opcion = prompt("1. Ver elementos de la cesta\n2. Agregar fruta a la cesta\n 3. eliminar fruta a la cesta\n 4. Salir ")
 
 
 
+
+
+}while(i<=cesta.length)
 
 
 
