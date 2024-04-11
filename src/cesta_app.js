b7139760a74c24b29ede00a47f519e236dd8250a
AutumnTiger2878 <franmora2783@gmail.com>
2024-04-11 08:35:21 -0600

    Practica #7

--- src/cesta_app.js
+++ src/cesta_app.js
@@ -1,48 +1,45 @@
 document.querySelector('#header').innerHTML = "Estoy en el documento array"
 
-
-
-
-
-function VerCesta(){
-    prompt(cesta)
-
-}
-function Agregar(){
-
-
-
-
-}
-function Eliminar(){
-
-
-
-
-}
-
-
-
-
-
-
-
-i = 0
-
 var cesta = []
 
+function ver() {
+    alert(cesta)
+}
+function agregar() {
+    var agregar = prompt("Digite una nueva fruta")
+    alert(cesta.push(agregar))
+}
+function quitar() {
+    cesta.pop()
+    alert("Se elimino correctamente")
+}
+var inicio = true
+do {
+    var opcion = prompt("1. Ver elementos de la cesta\n2. Agregar fruta a la cesta\n3. eliminar fruta a la cesta\n4. Salir ")
+
+    switch (parseInt(opcion)) {
+        case 1:
+            ver()
+            break;
+        case 2:
+            agregar()
+            break;
+        case 3:
+            quitar()
+            break
+        case 4:
+            inicio = false
+            break
+        default:
+            alert("Esa no es una opcion")
+            break
+
+
+    }
 
 
 
-
-do{
-var opcion = prompt("1. Ver elementos de la cesta\n2. Agregar fruta a la cesta\n 3. eliminar fruta a la cesta\n 4. Salir ")
-
-
-
-
-
-}while(i<=cesta.length)
+} while (inicio == true)
 
 
 
